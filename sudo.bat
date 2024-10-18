59e91c3388cc46947abb91caf2ab4cf0d2335032
SebastianLozanoValle <cesarloco2003@hotmail.com>
2024-10-17 21:34:07 -0500

    Ultimo Commit

--- sudo.bat
+++ sudo.bat
@@ -1,8 +1,18 @@
+@REM git add .
+@REM git commit -m "Ultimo Commit"
+@REM git push
+@REM set GOOS=linux
+@REM set GOARCH=amd64
+@REM go build -tags lambda.norpc -o bootstrap main.go
+@REM del main.zip
+@REM tar.exe -a -cf main.zip main
 git add .
 git commit -m "Ultimo Commit"
 git push
+
 set GOOS=linux
 set GOARCH=amd64
-go build -tags lambda.norpc -o bootstrap main.go
-del main.zip
-tar.exe -a -cf main.zip main
+go build -tags lambda.norpc -o bootstrap main.go  # Esto genera el archivo 'bootstrap'
+
+del main.zip  # Elimina el archivo 'main.zip' anterior si existe
+tar.exe -a -cf main.zip bootstrap  # Aquí debes agregar 'bootstrap' al archivo ZIP, no 'main'
